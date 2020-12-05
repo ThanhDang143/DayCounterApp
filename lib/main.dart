6cf6e15205f159cc863bd0b38defea52c75241f0
ThanhDang143 <ThanhDang143@users.noreply.github.com>
2020-12-05 08:36:30 +0700

    Finishhhhhh

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:daycounter/views/About.dart';
-import 'package:daycounter/views/Countdown.dart';
 import 'package:daycounter/views/Homepage.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: About(),
+      home: HomePage(),
     );
   }
 }
